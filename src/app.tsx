d3e7d4e814839eea4a085ad89856561790de7d75
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-30 12:39:29 -0300

    feat: add footer layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,20 @@
-import { ChevronLeft, ChevronRight, Home, Library, Play, Search } from 'lucide-react';
+import {
+	ChevronLeft,
+	ChevronRight,
+	Home,
+	Laptop2,
+	LayoutList,
+	Library,
+	Maximize2,
+	Mic2,
+	Play,
+	Repeat,
+	Search,
+	Shuffle,
+	SkipBack,
+	SkipForward,
+	Volume
+} from 'lucide-react';
 
 export function App() {
 	return (
@@ -41,6 +57,7 @@ export function App() {
 						</a>
 					</nav>
 				</aside>
+
 				<main className="flex-1 p-6">
 					<div className="flex items-center gap-4">
 						<button type="button" className="p-1 rounded-full bg-black/40">
@@ -55,7 +72,7 @@ export function App() {
 
 					<div className="grid grid-cols-3 gap-4 mt-4">
 						<a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px] object-cover" />
 							<strong>High Live</strong>
 							<button
 								type="button"
@@ -65,7 +82,7 @@ export function App() {
 							</button>
 						</a>
 						<a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px] object-cover" />
 							<strong>High Live</strong>
 							<button
 								type="button"
@@ -75,7 +92,7 @@ export function App() {
 							</button>
 						</a>
 						<a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px] object-cover" />
 							<strong>High Live</strong>
 							<button
 								type="button"
@@ -85,7 +102,7 @@ export function App() {
 							</button>
 						</a>
 						<a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px] object-cover" />
 							<strong>High Live</strong>
 							<button
 								type="button"
@@ -95,7 +112,7 @@ export function App() {
 							</button>
 						</a>
 						<a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px] object-cover" />
 							<strong>High Live</strong>
 							<button
 								type="button"
@@ -105,7 +122,7 @@ export function App() {
 							</button>
 						</a>
 						<a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-[104px] object-cover" />
 							<strong>High Live</strong>
 							<button
 								type="button"
@@ -120,34 +137,76 @@ export function App() {
 
 					<div className="grid grid-cols-5 gap-4 mt-4">
 						<a href="#" className="flex flex-col gap-2 bg-white/5 p-3 rounded-lg hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px] object-cover" />
 							<strong className="font-semibold">Daily Mix 1</strong>
 							<span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
 						</a>
 						<a href="#" className="flex flex-col gap-2 bg-white/5 p-3 rounded-lg hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px] object-cover" />
 							<strong className="font-semibold">Daily Mix 1</strong>
 							<span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
 						</a>
 						<a href="#" className="flex flex-col gap-2 bg-white/5 p-3 rounded-lg hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px] object-cover" />
 							<strong className="font-semibold">Daily Mix 1</strong>
 							<span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
 						</a>
 						<a href="#" className="flex flex-col gap-2 bg-white/5 p-3 rounded-lg hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px] object-cover" />
 							<strong className="font-semibold">Daily Mix 1</strong>
 							<span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
 						</a>
 						<a href="#" className="flex flex-col gap-2 bg-white/5 p-3 rounded-lg hover:bg-white/10 transition-colors">
-							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px]" />
+							<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="w-full h-[120px] object-cover" />
 							<strong className="font-semibold">Daily Mix 1</strong>
 							<span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
 						</a>
 					</div>
 				</main>
 			</div>
-			<footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
+
+			<footer className="flex items-center justify-between bg-zinc-800 border-t border-zinc-700 px-6 py-4">
+				<div className="flex items-center gap-3">
+					<img src="/album.jpg" alt="Capa do álbum High Live da banda Helloween" className="size-14 object-cover" />
+					<div className="flex flex-col">
+						<strong className="font-normal">Why?</strong>
+						<span className="text-xs text-zinc-400">Helloween</span>
+					</div>
+				</div>
+
+				<div className="flex flex-col items-center gap-2">
+					<div className="flex items-center gap-3">
+						<Shuffle size={20} className="text-zinc-200" />
+						<SkipBack size={20} className="text-zinc-200" />
+						<button type="button" className="flex items-center justify-center pl-0.5 size-10 rounded-full bg-white text-black">
+							<Play />
+						</button>
+						<SkipForward size={20} className="text-zinc-200" />
+						<Repeat size={20} className="text-zinc-200" />
+					</div>
+
+					<div className="flex items-center gap-2">
+						<span className="text-xs text-zinc-400">0:31</span>
+						<div className="h-1 rounded-full w-96 bg-zinc-600">
+							<div className="w-40 h-1 rounded-full bg-zinc-200" />
+						</div>
+						<span className="text-xs text-zinc-400">2:14</span>
+					</div>
+				</div>
+
+				<div className="flex items-center gap-4">
+					<Mic2 size={20} />
+					<LayoutList size={20} />
+					<Laptop2 size={20} />
+					<div className="flex items-center gap-2">
+						<Volume size={20} />
+						<div className="h-1 rounded-full w-24 bg-zinc-600">
+							<div className="w-10 h-1 rounded-full bg-zinc-200" />
+						</div>
+					</div>
+					<Maximize2 size={20} />
+				</div>
+			</footer>
 		</div>
 	);
 }
